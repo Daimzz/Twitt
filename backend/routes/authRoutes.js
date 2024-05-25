b5a9f2fd5c86d92e2df0760d097a587b55488ef6
Dmitry Moiseev <shanksdaymos@gmail.com>
2024-05-25 22:25:25 +0300

    added user routes and controllers

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import {Router} from "express"
-import {login, logout, signup} from "../controllers/authController.js";
+import {getMe, login, logout, signup} from "../controllers/authController.js";
+import {protectRoute} from "../middleware/protectRoute.js";
 
 const router = Router()
 
+router.get("/me", protectRoute, getMe )
 router.post("/signup", signup )
 router.post("/login", login)
 router.post("/logout", logout)

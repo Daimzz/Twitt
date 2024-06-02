c4d047b2692a1336f142437dd546fbac7c9b95c2
Dmitry Moiseev <shanksdaymos@gmail.com>
2024-06-02 15:14:55 +0300

    3 - auth handled and routes protected  in client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/auth/home/HomePage.jsx";
 import SignUpPage from "./pages/auth/signup/SignUpPage.jsx";
 import LoginPage from "./pages/auth/login/LoginPage.jsx";
@@ -6,19 +6,50 @@ import NotificationPage from "./pages/notification/NotificationPage.jsx";
 import ProfilePage from "./pages/profile/ProfilePage.jsx";
 import {Sidebar} from "./components/common/Sidebar.jsx";
 import {RightPanel} from "./components/common/RightPanel.jsx";
+import {Toaster} from "react-hot-toast";
+import {useQuery} from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner.jsx";
+
 function App() {
-    return (
-        <div className='flex max-w-6xl mx-auto'>
-            <Sidebar/>
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/signup' element={<SignUpPage />} />
-                <Route path='/login' element={<LoginPage />} />
-                <Route path='/notifications' element={<NotificationPage />} />
-                <Route path='/profile/:username' element={<ProfilePage />} />
-            </Routes>
-            <RightPanel/>
-        </div>
-    );
+	const {data: authUser, isLoading} = useQuery({
+		queryKey: ['authUser'],
+		queryFn: async () => {
+			try {
+				const res = await fetch('/api/auth/me')
+				const data = await res.json()
+				if(data.error) return null
+				if (!res.ok || data.error) {
+					throw new Error(data.error || 'Something went wrong!')
+				}
+				console.log('authUser is here: ', data)
+				return data
+			} catch (error) {
+				throw new Error(error)
+			}
+		},
+		retry: false
+	})
+
+	if (isLoading) {
+		return <div className={' h-screen flex items-center justify-center'}>
+			<LoadingSpinner size={'lg'}/>
+		</div>
+	}
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			{authUser && <Sidebar/>}
+			<Routes>
+				<Route path='/' element={authUser ? <HomePage/> : <Navigate to={'/login'}/>}/>
+				<Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to={'/'}/>}/>
+				<Route path='/signup' element={!authUser ? <SignUpPage/> : <Navigate to={'/'}/>}/>
+				<Route path='/notifications' element={authUser ? <NotificationPage/> : <Navigate to={'/login'}/>}/>
+				<Route path='/profile/:username' element={authUser ? <ProfilePage/> : <Navigate to={'/login'}/>}/>
+			</Routes>
+			{authUser && <RightPanel/>}
+			<Toaster/>
+
+		</div>
+	);
 }
+
 export default App
--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -1,68 +1,95 @@
-
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import {useState} from "react";
+import {Link} from "react-router-dom";
 
 import XSvg from "../../../components/svgs/X";
 
-import { MdOutlineMail } from "react-icons/md";
-import { MdPassword } from "react-icons/md";
+import {MdOutlineMail} from "react-icons/md";
+import {MdPassword} from "react-icons/md";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 
 const LoginPage = () => {
 	const [formData, setFormData] = useState({
 		username: "",
 		password: "",
 	});
+	const queryClient = useQueryClient();
+
+	const {mutate: loginMutation, isError, error, isPending} = useMutation({
+		mutationFn: async ({username, password}) => {
+			try {
+				const res = await fetch("/api/auth/login", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({username, password}),
+				});
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || "Failed to login in account")
+				return data
+			} catch (error) {
+				throw new Error(error)
+			}
+
+		},
+
+		onSuccess: () => {
+			//refetch authUser
+			queryClient.invalidateQueries({queryKey: ['authUser']})
+		},
+
+	})
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(formData);
+		loginMutation(formData);
 	};
 
 	const handleInputChange = (e) => {
-		setFormData({ ...formData, [e.target.name]: e.target.value });
+		setFormData({...formData, [e.target.name]: e.target.value});
 	};
 
-	const isError = false;
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen'>
 			<div className='flex-1 hidden lg:flex items-center  justify-center'>
-				<XSvg className='lg:w-2/3 fill-white' />
+				<XSvg className='lg:w-2/3 fill-white'/>
 			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='flex gap-4 flex-col' onSubmit={handleSubmit}>
 					<div className={'flex justify-center items-center'}>
 						<XSvg className='w-24 lg:hidden fill-white'/>
 					</div>
-						<h1 className='text-4xl font-extrabold text-white text-center'>{"Let's"} go.</h1>
-						<label className='input input-bordered rounded flex items-center gap-2'>
-							<MdOutlineMail/>
-							<input
-								type='text'
-								className='grow'
-								placeholder='username'
-								name='username'
-								onChange={handleInputChange}
-								value={formData.username}
-							/>
-						</label>
+					<h1 className='text-4xl font-extrabold text-white text-center'>{"Let's"} go.</h1>
+					<label className='input input-bordered rounded flex items-center gap-2'>
+						<MdOutlineMail/>
+						<input
+							type='text'
+							className='grow'
+							placeholder='username'
+							name='username'
+							onChange={handleInputChange}
+							value={formData.username}
+						/>
+					</label>
 
-						<label className='input input-bordered rounded flex items-center gap-2'>
-							<MdPassword/>
-							<input
-								type='password'
-								className='grow'
-								placeholder='Password'
-								name='password'
-								onChange={handleInputChange}
-								value={formData.password}
-							/>
-						</label>
-						<button className='btn rounded-full btn-primary text-white'>Login</button>
-						{isError && <p className='text-red-500'>Something went wrong</p>}
+					<label className='input input-bordered rounded flex items-center gap-2'>
+						<MdPassword/>
+						<input
+							type='password'
+							className='grow'
+							placeholder='Password'
+							name='password'
+							onChange={handleInputChange}
+							value={formData.password}
+						/>
+					</label>
+					<button
+						className='btn rounded-full btn-primary text-white'>{isPending ? "Loading..." : "Login"}</button>
+					{isError && <p className='text-red-500'>{error?.message || "Something went wrong with login"}</p>}
 				</form>
 				<div className='flex flex-col gap-2 mt-4'>
-				<p className='text-white text-lg'>{"Don't"} have an account?</p>
+					<p className='text-white text-lg'>{"Don't"} have an account?</p>
 					<Link to='/signup'>
 						<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Sign up</button>
 					</Link>
